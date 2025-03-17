c911e2c78a254a3f5f38896179f267066590fc5f
Java20091@ <jidedorcas@gmail.com>
2025-03-17 20:51:55 +0100

    multiple users

--- src/main/java/com/pks/spring/security_6/config/SecurityConfig.java
+++ src/main/java/com/pks/spring/security_6/config/SecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,4 +30,22 @@ public class SecurityConfig {
     return http.build();
 
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails jide = User
+                .withUsername("jide")
+                        .password("{noop}password")
+                                .roles("USER")
+                                        .build();
+        UserDetails nikky = User
+                .withUsername("nikky")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(jide,nikky);
+    }
+
+
 }
